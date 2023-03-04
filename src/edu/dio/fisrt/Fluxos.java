5a918f79868ac41fb3959598d68a257bfa671a70
lucabDev <luccacpm@gmail.com>
2023-03-04 00:39:44 -0300

    feat: fluxos

---
+++ src/edu/dio/fisrt/Fluxos.java
@@ -0,0 +1,16 @@
+package edu.dio.fisrt;
+
+public class Fluxos {
+    
+    public static void main(String[] args) {
+        
+        ifArrow();
+        ifArrowness();
+        ifSmallest();
+        ifVacation();
+
+        switchWeek();
+        switchNumber();
+        switchVacation();
+    }
+}
